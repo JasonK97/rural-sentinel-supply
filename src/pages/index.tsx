64a9ad00cc54e42b89e997420d58623952220bad
Jason Kent <jasonbkent@gmail.com>
2023-05-24 15:23:05 -0600

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { GetStaticProps } from "next";
+import Link from "next/link";
 import shuffle from "lodash.shuffle";
 
 import { printful } from "../lib/printful-client";
@@ -28,10 +29,11 @@ const IndexPage: React.FC<IndexPageProps> = ({ products }) => (
               <p className="max-w-3xl mx-auto mb-10 text-lg text-gray-300">
                 Emergency Preparedness. Self-Defense. Second Amendment. Hunting.
               </p>
-              <a className="inline-block w-full md:w-auto mb-4 md:mr-6 py-5 px-8 text-sm font-bold uppercase border-2 border-transparent bg-gray-200 rounded hover:bg-gray-100 text-gray-800 transition duration-200"
-                href="/soft-goods">
+<Link to="/soft-goods">
+              <a className="inline-block w-full md:w-auto mb-4 md:mr-6 py-5 px-8 text-sm font-bold uppercase border-2 border-transparent bg-gray-200 rounded hover:bg-gray-100 text-gray-800 transition duration-200">
                   View Soft Goods
                 </a>
+<Link />
             </div>
           </div>
         </div>
