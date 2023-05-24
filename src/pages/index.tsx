ac1ec7329b5f5482cf4a652e0e19c258b8a89474
Jason Kent <jasonbkent@gmail.com>
2023-05-24 15:45:56 -0600

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,7 @@ const IndexPage: React.FC<IndexPageProps> = ({ products }) => (
               <a className="inline-block w-full md:w-auto mb-4 md:mr-6 py-5 px-8 text-sm font-bold uppercase border-2 border-transparent bg-gray-200 rounded hover:bg-gray-100 text-gray-800 transition duration-200">
                   View Soft Goods
                 </a>
-<Link />
+</Link>
             </div>
           </div>
         </div>
