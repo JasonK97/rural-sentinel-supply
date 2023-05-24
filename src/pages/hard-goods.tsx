437bc307d97e6f68371af157f106f55cefb3370e
Jason Kent <jasonbkent@gmail.com>
2023-05-24 14:56:39 -0600

    link updates

--- src/pages/hard-goods.tsx
+++ src/pages/hard-goods.tsx
@@ -6,11 +6,11 @@ function HardGoodsPage() {
       <div className="max-w-xl mx-auto prose prose-blue">
         <h2>Field Journals</h2>
         <ul>
-          <li><a href='https://www.amazon.com/dp/B0C2RSC422' target='_blank'>Field Journal No. 004 - Olive Drab</a></li>
-          <li><a href='https://www.amazon.com/dp/B0C2RVJL1G' target='_blank'>Field Journal No. 010 - Topographical Olive Drab</a></li>
-          <li><a href='https://www.amazon.com/dp/B0C47JCYVD' target='_blank'>Field Journal No. 011 - Topographical Coyote</a></li>
-          <li><a href='https://www.amazon.com/dp/B0C47YQY5R' target='_blank'>Field Journal No. 012 - Topographical Navy</a></li>
-          <li><a href='https://www.amazon.com/dp/B0C47LSDHW' target='_blank'>Field Journal No. 013 - Topographical Midnight</a></li>
+          <li><a href='https://www.amazon.com/dp/B0C2RSC422' target='_blank' rel="noreferrer">Field Journal No. 004 - Olive Drab</a></li>
+          <li><a href='https://www.amazon.com/dp/B0C2RVJL1G' target='_blank' rel="noreferrer">Field Journal No. 010 - Topographical Olive Drab</a></li>
+          <li><a href='https://www.amazon.com/dp/B0C47JCYVD' target='_blank' rel="noreferrer">Field Journal No. 011 - Topographical Coyote</a></li>
+          <li><a href='https://www.amazon.com/dp/B0C47YQY5R' target='_blank' rel="noreferrer">Field Journal No. 012 - Topographical Navy</a></li>
+          <li><a href='https://www.amazon.com/dp/B0C47LSDHW' target='_blank' rel="noreferrer">Field Journal No. 013 - Topographical Midnight</a></li>
         </ul>
       </div>
       <div className="flex justify-around m-8">
