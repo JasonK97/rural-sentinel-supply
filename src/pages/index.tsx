90170e9790e80b6eb1c4e7881fef3df5c4d94017
Jason Kent <jasonbkent@gmail.com>
2023-10-20 22:51:40 -0600

    Update index.tsx for RSN01 Pattern Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,8 +17,8 @@ const IndexPage: React.FC<IndexPageProps> = () => (
     />
 
     {/* <div className="w-full bg-center bg-cover bg-[url('/unsplash/abigail-loney-unsplash.jpg')]"> */}
-    {/* <div className="w-full bg-center bg-cover bg-[url('/RSN01-xl.png')]"> */}
-    <div className="w-full bg-center bg-cover bg-[url('/runes-banner.png')]">
+    <div className="w-full bg-center bg-cover bg-[url('/RSN01-xl.png')]">
+    {/* <div className="w-full bg-center bg-cover bg-[url('/runes-banner.png')]"> */}
       <div className="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50 py-12">
         <div className="text-center">
           <div className="container px-4 mx-auto">
@@ -43,11 +43,10 @@ const IndexPage: React.FC<IndexPageProps> = () => (
                 Rural Sentinel Supply
               </span>
               <h2 className="mt-8 mb-6 text-4xl lg:text-5xl font-bold text-gray-100">
-                The Aegishjalmur and Heimdall.
+                RSN01 Pattern Camo.
               </h2>
               <p className="max-w-3xl mx-auto mb-10 text-lg text-gray-300">
-                Used to protect warriors and intill fear in their enemies.<br />
-                Emanate Heimdall, the Father of Mankind and Guardian of Asgard.
+                Up your Camo Game today.
               </p>
               <Link href="/soft-goods">
                 <a className="inline-block w-full md:w-auto mb-4 md:mr-6 py-5 px-8 text-sm font-bold uppercase border-2 border-transparent bg-gray-200 rounded hover:bg-gray-100 text-gray-800 transition duration-200">
