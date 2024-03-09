f2429527232800824d2738f471cb7703a316b72b
Jason Kent <jasonbkent@gmail.com>
2024-03-08 21:05:54 -0700

    apostraphe fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,7 +46,7 @@ const IndexPage: React.FC<IndexPageProps> = () => (
                 For the Impirium of Man!
               </h2>
               <p className="max-w-3xl mx-auto mb-10 text-lg text-gray-300">
-                Warhammer 40k Space Marines bringing the Emperor's rule to the galaxy.
+                Warhammer 40k Space Marines bringing the Emperor&apos;s rule to the galaxy.
               </p>
               <Link href="/soft-goods">
                 <a className="inline-block w-full md:w-auto mb-4 md:mr-6 py-5 px-8 text-sm font-bold uppercase border-2 border-transparent bg-gray-200 rounded hover:bg-gray-100 text-gray-800 transition duration-200">
