ad1653d720d0a021d023f934a0927106b5a98a12
Jason Kent <jasonbkent@gmail.com>
2024-09-03 16:57:11 -0600

    Taking a break

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -137,7 +137,7 @@ const Layout = ({ children }) => {
                   </svg>
                 </a>
               </Link>
-                <button
+                {/* <button
                   className="snipcart-checkout appearance-none px-2 text-gray-800 hover:text-slate-400 rounded-md cursor-pointer focus:outline-none focus:text-slate-400 transition relative"
                   aria-label="Cart"
                 >
@@ -152,7 +152,7 @@ const Layout = ({ children }) => {
                     <path fill="none" d="M0 0h24v24H0z" />
                     <path d="M4 16V4H2V2h3a1 1 0 0 1 1 1v12h12.438l2-8H8V5h13.72a1 1 0 0 1 .97 1.243l-2.5 10a1 1 0 0 1-.97.757H5a1 1 0 0 1-1-1zm2 7a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm12 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4z" />
                   </svg>
-              </button>
+              </button> */}
             </div>
           </div>
         </div>
